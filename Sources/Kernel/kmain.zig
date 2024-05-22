46741e85fe49251477a36a6724665865b547e8cd
Kbz-8 <kbz_8.dev@akel-engine.com>
2024-05-22 20:32:16 +0200

    black magic has been used, removing assmbly code, working on build file

--- Sources/Kernel/kmain.zig
+++ Sources/Kernel/kmain.zig
@@ -1,1 +1,30 @@
-export fn kernel_main() void {}
+const ALIGN = 1 << 0;
+const MEMINFO = 1 << 1;
+const MAGIC = 0x1BADB002;
+const FLAGS = ALIGN | MEMINFO;
+
+const MultiBoot = packed struct {
+    magic: i32,
+    flags: i32,
+    checksum: i32,
+};
+
+export var multiboot align(4) linksection(".multiboot") = MultiBoot{
+    .magic = MAGIC,
+    .flags = FLAGS,
+    .checksum = -(MAGIC + FLAGS),
+};
+
+export var stack: [16 * 1024]u8 align(16) linksection(".bss") = undefined;
+
+export fn _start() callconv(.Naked) noreturn {
+    @call(.{ .stack = &stack }, kernel_main, .{});
+    while (true)
+        asm volatile ("hlt");
+}
+
+fn kernel_main() void {
+    const vga_buffer: [*]u8 = @ptrFromInt(0xB8000);
+    vga_buffer[0] = '4';
+    vga_buffer[1] = '2';
+}
--- build.zig
+++ build.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void
-{
+pub fn build(b: *std.Build) void {
+    const os = b.addExecutable(.{
+        .name = "image.elf",
+        .root_source_file = .{ .path = "Sources/Kernel/kmain.zig" },
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .x86,
+            .os_tag = .freestanding,
+        }),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+    os.setLinkerScriptPath(.{ .path = "linker.ld" });
+    b.installArtifact(os);
+
+    const run_cmd = b.addSystemCommand(&.{
+        "qemu-system-i386",
+        "-kernel",
+        "zig-out/bin/image.elf",
+    });
+
+    const run_step = b.step("run", "Run the os");
+    run_step.dependOn(&run_cmd.step);
 }
